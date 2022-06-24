23db4dd23d1280900db503ee45b88d58ebc7a19f
sufi <sufiyanshaikh11919@gmail.com>
2022-06-23 22:45:01 -0700

    added js to fetch API to display images

--- Script/script.js
+++ Script/script.js
@@ -1,6 +1,8 @@
 const puma = document.getElementById("puma");
+const nike = document.getElementById("nike");
+const adidas = document.getElementById("adidas");
 
-fetch("https://jsonplaceholder.typicode.com/photos?_limit=10").then((data)=>{
+fetch("https://jsonplaceholder.typicode.com/photos?_limit=15").then((data)=>{
     // console.log(data);
     return data.json();
 }).then((fetched_data)=>{
@@ -14,11 +16,34 @@ fetch("https://jsonplaceholder.typicode.com/photos?_limit=10").then((data)=>{
     })
 })
 
-    // }).then((fetch_data)=>{
-//     fetch_data.map(value =>{
-//         key = value.id;
-//         let img = doument.createElement('img');
-//         img.src = value.url
-//         puma.appendChild(img)
-//     })
-// });
+
+
+
+fetch("https://jsonplaceholder.typicode.com/photos?_limit=15").then((data)=>{
+    return data.json();
+}).then((fetched_data)=>{
+    fetched_data.map(value=>{
+        key = value.id;
+        let img = document.createElement("img");
+        img.src = value.url;
+        nike.appendChild(img)
+        // console.log(value.url)
+    })
+})
+
+
+
+fetch("https://jsonplaceholder.typicode.com/photos?_limit=15").then((data)=>{
+    // console.log(data);
+    return data.json();
+}).then((fetched_data)=>{
+    // console.log(fetched_data[0].url);
+    fetched_data.map(value=>{
+        key = value.id;
+        let img = document.createElement("img");
+        img.src = value.url;
+        adidas.appendChild(img)
+        // console.log(value.url)
+    })
+})
+
